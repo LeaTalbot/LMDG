e96dae321a1836538aa9ce64255ac8df8a79df70
Lea <talbot.lea@gmail.com>
2017-01-08 23:16:45 -0500

    First commit + inventory button (+ reminder: let's not forget to ignore temp files on the repo when they start showing up)

---
+++ Assets/Script/UI/ShowInventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowInventory : MonoBehaviour {
+
+
+
+	// this image is the row of blank inventory space
+	public Image imageInvUI;
+
+	private bool inventoryIsActive = false;
+
+
+	// Switch visibility of the inventory
+	public void OnClick () {
+
+		if (inventoryIsActive == true) {
+			
+			imageInvUI.enabled = false;
+			inventoryIsActive = false;
+
+		} else if (inventoryIsActive == false) {
+			
+			imageInvUI.enabled = true;
+			inventoryIsActive = true;
+		}
+	}
+}
