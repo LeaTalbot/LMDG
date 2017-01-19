182d702cbc5ab8d8ae558ce74a71153e39c26bf8
Lea <talbot.lea@gmail.com>
2017-01-19 18:15:12 -0500

    Walking with arrows added + animation

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	Rigidbody2D myBody;
+
+	private Animator animator;
+
+	public float playerSpeed = 5;
+
+	public float currentXSpeed;
+	public float currentYSpeed;
+
+
+
+	void Start () {
+	
+		myBody = GetComponent<Rigidbody2D>();
+		myBody.velocity = new Vector2(0, 0);
+
+		animator = GetComponent<Animator>();
+	}
+	
+
+	void Update () {
+	
+		//MOVEMENT IMPLEMENTATION (NO DIAGONALS ALLOWED!)
+
+		//Still need to fix player so that we can go up and down...
+
+		//ARROW MOVEMENTS:
+
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+			currentYSpeed = 0;
+			currentXSpeed = -playerSpeed;
+		}
+		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+
+			currentYSpeed = 0;
+			currentXSpeed = playerSpeed;
+		}
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+
+			currentXSpeed = 0;
+			currentYSpeed = playerSpeed;
+		}
+		if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+
+			currentXSpeed = 0;
+			currentYSpeed = -playerSpeed;
+		}
+
+		//release the keys 
+
+		if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) {
+			
+			if (currentXSpeed < 0) {
+				currentXSpeed = 0;
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) {
+			
+			if (currentXSpeed > 0) {
+				currentXSpeed = 0;
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+
+			if (currentYSpeed > 0) {
+				currentYSpeed = 0;
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) {
+
+			if (currentYSpeed < 0) {
+				currentYSpeed = 0;
+			}
+		}
+			
+
+		// POINT AND CLICK MOVEMENTS:
+
+
+
+
+		myBody.velocity = new Vector2(currentXSpeed, currentYSpeed);
+
+
+		//WALKING AND IDLE ANIMATION
+
+		if (currentXSpeed == 0 && currentYSpeed == 0) {
+			
+			animator.Play("idle");
+		} 
+		else if (currentXSpeed < 0 && currentYSpeed == 0) {
+
+			animator.Play("walkLeft");
+		}
+		else if (currentXSpeed > 0 && currentYSpeed == 0) {
+
+			animator.Play("walkRight");
+		}
+		else if (currentYSpeed < 0 && currentXSpeed == 0) {
+
+			animator.Play("walkDown");
+		}
+		else if (currentYSpeed > 0 && currentXSpeed == 0) {
+
+			animator.Play("walkUp");
+		}
+	}
+}
